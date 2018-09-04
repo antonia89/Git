5c1c0e7e725ffe381e8f2ab2027b9907af0a3ac1
Antonia <antonia09@ru.is>
2018-09-04 18:40:55 +0000

    added to max_int file

--- max_int.py
+++ max_int.py
@@ -3,3 +3,13 @@
 #The user puts in a number
 #The user continues to put in numbers until a negative value is entered.
 #If a negative value is intered the program prints out the maximum positive intiger from the user.
+
+
+num_int = 0  # Do not change this line
+max_int = 0
+
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
