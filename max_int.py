852e9a68b268a25247c3b99e4a694623f5f8116c
Antonia <antonia09@ru.is>
2018-09-04 18:13:33 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -1,5 +1,5 @@
 
 
 #The user puts in a number
-#The user continues to put in numbers until a negative value is entered
-#If a negative value is intered the program prints out the maximum positive intiger from the user
+#The user continues to put in numbers until a negative value is entered.
+#If a negative value is intered the program prints out the maximum positive intiger from the user.
